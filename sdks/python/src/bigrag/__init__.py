e07bcc9282158478b14c0b6e7ef9ac63c2b4321f
Bigint <69431456+bigint@users.noreply.github.com>
2026-05-17 14:44:41 +0530

    chore(sdk-py): drop root-level Admin*Resource re-exports

--- sdks/python/src/bigrag/__init__.py
+++ sdks/python/src/bigrag/__init__.py
@@ -22,18 +22,7 @@ from bigrag._files import FileInput, normalize_file_input
 from bigrag._sse import parse_sse_stream
 from bigrag._version import __version__
 from bigrag.resources import (
-    AdminAccessResource,
-    AdminApiKeysResource,
-    AdminAuditResource,
-    AdminBackupsResource,
-    AdminConnectorsResource,
-    AdminEmbeddingPresetsResource,
-    AdminGoogleConnectorResource,
-    AdminMcpServersResource,
-    AdminRealtimeResource,
     AdminResource,
-    AdminSettingsResource,
-    AdminUsersResource,
     AuthResource,
     ChatResource,
     CollectionsResource,
@@ -71,18 +60,7 @@ __all__ = [
     # SSE
     "parse_sse_stream",
     # Resources
-    "AdminAccessResource",
-    "AdminApiKeysResource",
-    "AdminAuditResource",
-    "AdminBackupsResource",
-    "AdminConnectorsResource",
-    "AdminEmbeddingPresetsResource",
-    "AdminGoogleConnectorResource",
-    "AdminMcpServersResource",
-    "AdminRealtimeResource",
     "AdminResource",
-    "AdminSettingsResource",
-    "AdminUsersResource",
     "AuthResource",
     "ChatResource",
     "CollectionsResource",
