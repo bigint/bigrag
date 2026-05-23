04e59f5cb1b7ba1d730c80a68587aab0930dc4e4
Bigint <69431456+bigint@users.noreply.github.com>
2026-05-23 19:31:26 +0530

    fix: render S3 connector route correctly

--- app/src/routes/_dashboard.collections.$name.connectors.s3.tsx
+++ app/src/routes/_dashboard.collections.$name.connectors.s3.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, lazyRouteComponent } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_dashboard/collections/$name/connectors/s3")({
-  component: () =>
+  component: lazyRouteComponent(() =>
     import("@/features/collections/s3-connector-route").then((m) => ({
       default: m.S3Connector,
     })),
+  ),
 });
